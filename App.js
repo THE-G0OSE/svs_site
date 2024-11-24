43c64af544fa05a64158833e1f97171f9c134808
THE-G0OSE <nikitos54345@gmail.com>
2024-11-24 13:28:55 +0300

    Update App.js

--- App.js
+++ App.js
@@ -23,6 +23,9 @@ function App() {
   return (
 	  <>
 	  <head>
+	  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap" rel="stylesheet">
 	  </head>
    		<body>
 			<Header lang={lang} setEN={setEN} setRU={setRU}/>
